49a6750ca1391badf6fb6a11090dad66a554ef87
Alesson Arcanjo <alessonsarcanjo@gmail.com>
2025-09-16 14:11:03 +0000

    declareis as variaveis da carta 1

---
+++ SuperTrunfo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    //Declarando as variáveis
+    printf("Carta 01\n");
+    char estado01;
+    char codigo01[9];
+    char cidade01[20];
+    int população01;
+    float área01;
+    float PIB01;
+    int turistico01;
+
+}
